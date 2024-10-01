334f6c846460bc4c492795648c76591eff5b07fd
AlexM <alexandre.martins-f@outlook.com>
2024-10-01 20:50:19 +0200

    test

--- prefect_orchestration/hello.py
+++ prefect_orchestration/hello.py
@@ -1,7 +1,11 @@
-from prefect import flow, task, get_run_logger
+from prefect import flow, task
+
+@task(log_prints=True)
+def dummy():
+    print(3)
+
 
 @flow
-def my_flow(message: str):
-    logger = get_run_logger()
-    logger.info(message)
-
+def main():
+    for _ in range(1, 10):
+        dummy.submit()

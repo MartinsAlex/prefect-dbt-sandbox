edf11aa4eb9c757d27dd1cd9674308139bb4873e
AlexM <alexandre.martins-f@outlook.com>
2024-11-30 23:16:44 +0100

    flow

--- prefect_orchestration/hello.py
+++ prefect_orchestration/hello.py
@@ -1,11 +1,8 @@
 from prefect import flow, task
 
-@task(log_prints=True)
-def dummy():
-    print(3)
 
 
-@flow
-def my_flow():
-    for _ in range(1, 10):
-        dummy.submit()
+@flow(log_prints=True)
+def my_flow(message: str):
+    print(f"{message}")
+    
